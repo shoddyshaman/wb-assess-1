620046cc075586d11fbc88af556f0b706456eed6
shoddyshaman <dhananjoy.tiwari@gmail.com>
2024-06-10 15:46:39 -0700

    completed code

---
+++ src/arrays.js
@@ -0,0 +1,133 @@
+// Given an array of numbers, return all the numbers that are greater than ten.
+// Ex.:
+//   greaterThanTen([1, 2, 3, 11, 12, 13]);
+//   => [11, 12, 13]
+function greaterThanTen(numbers) {
+  const result = []
+  for(const num of numbers){
+    if(num > 10){
+      result.push(num)
+    }
+  }
+  return result
+}
+
+
+// Given an array of strings, return all words that start with 'b' or 'B'.
+// Ex.:
+//   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
+//   => ['banana', 'Bonobo]
+function bWords(words) {
+  const result = []
+  for(const word of words){
+    if(word[0] === 'b' || word[0] === 'B'){
+      result.push(word)
+    }
+  }
+  return result
+}
+
+// Add all the elements from additionalItems to the end of originalArray.
+// Return the originalArray..
+// Ex.:
+//  extend([1, 2, 3], [4, 5, 6]);
+//  => [1, 2, 3, 4, 5, 6]
+function extend(originalArray, additionalItems) {
+  for(const item of additionalItems){
+    originalArray.push(item)
+  }
+  return originalArray
+}
+
+// Return an array of all items with the given length.
+// Ex.:
+//   itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
+//   => ['bbb', 'eee']
+function itemsWithLength(items, length) {
+  const result = []
+  for(const item of items){
+    if(item.length === length){
+      result.push(item)
+    }
+  }
+  return result
+}
+
+// Return an array with every other element from the input array (start with index 0).
+// Ex.:
+//   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
+//   => ['a', 'c', 'e']
+function everyOtherItem(items) {
+  const result = []
+  for(let i = 0; i < items.length;i += 2){
+    result.push(items[i])
+  }
+  return result
+}
+
+// Given a list of words and a letter, return the indexes of the words that
+// start with that letter. You can assume that the words and letter will always
+// be lowercased.
+// Ex.:
+//   findWordsStartingWith(['apple', 'banana', 'kiwi', 'pear', 'bacon'], 'b');
+//   => [1, 4]
+function findWordsStartingWith(words, letter) {
+  const result = []
+  for(let i = 0; i < words.length;i++){
+    if(words[i][0] === letter){
+      result.push(i)
+    }
+  }
+  return result
+}
+
+// Return the `n` smallest values in the array in descending order (largest
+// numbers first). Assume that `n` will always be less than the length of the
+// array.
+// Ex.:
+//   smallestNItems([1, 30, 4, 21, 100000], 3);
+//   => [21, 4, 1]
+function smallestNItems(items, n) {
+  items.sort((a,b) => b - a)
+  return items.slice(items.length - n)
+}
+
+// Search for a value in the array and return its index. If the value appears
+// more than once, return the index of the *FIRST* occurrence of the value. If
+// the value doesn't exist in the array, return undefined.
+// Ex.:
+//   findIndex(['a', 'b', 'c', 'a', 'b', 'c'], 'c');
+//   => 2
+function findIndex(items, value) {
+  for(let i = 0;i < items.length;i++){
+    if(items[i] === value){
+      return i
+    }
+  }
+  return undefined
+}
+
+// Given a start number and stop number, return a new array containing all the numbers
+// between the start and stop number.
+// Ex.:
+//   range(1, 5);
+//   => [1, 2, 3, 4, 5]
+function range(start, stop) {
+  const result = []
+  for(let i = start;i <= stop;i++){
+    result.push(i)
+  }
+  return result
+}
+
+export {
+  bWords,
+  everyOtherItem,
+  extend,
+  findIndex,
+  findWordsStartingWith,
+  greaterThanTen,
+  itemsWithLength,
+  range,
+  smallestNItems,
+};
---
+++ src/functions.js
@@ -0,0 +1,121 @@
+// Return the quotient of x and y.
+// Ex.:
+//   divide(10, 2);
+//   => 5
+function divide(x, y) {
+  return x/y
+}
+
+// Return the average of x and y.
+// Here, the average of x and y is the *mean* of x and y. In other words, it's
+// computed by dividing the sum of x and y by 2.
+// Ex.:
+//   average(10, 2);
+//   => 6
+function average(x, y) {
+  return (x + y) / 2
+}
+
+// Return true if x and y are mostly equivalent up to a tolerance of 0.001.
+// In other words, return true if the aboslute value of x - y is less than 0.001.
+// Ex.:
+//   approximatelyEqual(10.001, 10);
+//   => true
+function approximatelyEqual(x, y) {
+  return Math.abs(x - y) < 0.001
+}
+
+// Given a first name and last name, return a full name in the format "FIRST LAST"
+// Ex.:
+//   fullName('John', 'Doe');
+//   => 'John Doe'
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`
+}
+
+// Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
+// person, beverage and location provided.
+// Ex.:
+//   generateSentence('Kay', 'coffee', 'the local cafe');
+//   => 'Kay was drinking coffee at the local cafe.'
+function generateSentence(person, beverage, location) {
+  return `${person} was drinking ${beverage} at ${location}.`
+}
+
+// Return the given string with all vowels replced with '*' characters.
+// Ex.:
+//   censorVowels('javascript');
+//   => 'j*v*scr*pt'
+function censorVowels(string) {
+  const vowels = ['a','e','i','o','u']
+  let result = ''
+  for(const char of string){
+    if(vowels.includes(char)){
+      result += '*'
+    } else {
+      result += char
+    }
+  }
+  return result
+}
+
+// Return the given string in sticky case.
+// Ex.:
+//   stickyCase('hello world');
+//   => 'hElLo wOrLd'
+function stickyCase(string) {
+  let result = ''
+  for(let i = 0;i < string.length;i++){
+    if(i % 2 === 0){
+      result += string[i].toLowerCase()
+    } else {
+      result += string[i].toUpperCase()
+    }
+  }
+  return result
+}
+
+// Return the given string in leetspeak. Leetspeak is a modified version of
+// English where characters are replaced by numbers or symbols. For this
+// version of leetspeak, replace the following characters:
+//   a => 4
+//   e => 3
+//   i => 1
+//   o => 0
+//   s => 5
+//   t => 7
+// Ex.:
+//   leetspeak('javascript');
+//   => 'j4v45cr1p7'
+function leetspeak(string) {
+  let result = ''
+  for(const char of string){
+    if(char === 'a'){
+      result += '4'
+    } else if (char === 'e'){
+      result += '3'
+    } else if (char === 'i'){
+      result += '1'
+    } else if (char === 'o'){
+      result += '0'
+    } else if (char === 's'){
+      result += '5'
+    } else if (char === 't'){
+      result += '7'
+    } else {
+      result += char
+    }
+  }
+  return result
+}
+
+export {
+  approximatelyEqual,
+  average,
+  censorVowels,
+  divide,
+  fullName,
+  generateSentence,
+  leetspeak,
+  stickyCase,
+};
